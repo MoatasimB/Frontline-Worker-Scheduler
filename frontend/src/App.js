9c94eea61bd79070069f93bd74f5d7388a00f526
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2024-11-21 22:57:08 -0500

    login ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+        display: 'flex',          // Enable Flexbox
+        justifyContent: 'center', // Horizontal alignment
+        alignItems: 'center',     // Vertical alignment
+        width: '100vw',           // Full viewport width
+        height: '100vh',          // Full viewport height
+    }}>
+      <Login/>
     </div>
   );
 }
